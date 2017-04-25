e09be63b4a0019eb5fab49ae0d50535d87a15c60
abidibo <abidibo@gmail.com>
2017-04-25 11:52:16 +0200

    code refactoring to support more widget types

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
-import moment from 'moment'
 import parse from 'utils/parse'
 import { execFunctionByName } from 'utils/functions'
+import DefaultTMRealtimeWidget from 'widgets/default'
 
 export default class TMWidgets {
-
-  defaultLabels = {
-    lastMeasure: 'ultima rilevazione',
-    temperature: 'temperatura',
-    rh: 'umidità relativa',
-    pressure: 'pressione',
-    wind: 'vento',
-    rainRate: 'intensità precipitazioni',
-    rain: 'accumulo precipitazioni'
-  }
 
   run () {
     parse('tmrealtime', this.initTMRealtime, this)
@@ -32,93 +22,24 @@ export default class TMWidgets {
     })
   }
 
-  label (el, l) {
-    return $(el).attr(l + 'Label') || this.defaultLabels[l]
-  }
-
-  windDirClass (dir) {
-    if (dir > 337) {
-      return 'direction-up'
-    } else if (dir > 293) {
-      return 'direction-up-left'
-    } else if (dir > 248) {
-      return 'direction-left'
-    } else if (dir > 202) {
-      return 'direction-down-left'
-    } else if (dir > 158) {
-      return 'direction-down'
-    } else if (dir > 113) {
-      return 'direction-down-right'
-    } else if (dir > 68) {
-      return 'direction-right'
-    } else if (dir > 23) {
-      return 'direction-up-right'
-    } else {
-      return 'direction-up'
-    }
-  }
-
   renderTMRealtime (data, el) {
     // attributes
-    let datetimeFormat = $(el).attr('datetimeFormat') || 'LLL'
-    let stationSlug = $(el).attr('station')
-    // classes
-    let tempClass = data.temperature < 10 ? 'cold' : (data.temperature < 20 ? 'warm' : 'hot')
-    // dom
-    let wrapper = $('<section />', { 'class': 'tm-realtime-widget' })
-    let tpl = `
-      <header>
-        <div style="background: #000">
-          <a href="//www.torinometeo.org/realtime/${stationSlug}" target="_blank">
-            <img src="https://www.torinometeo.org/static/core/src/img/logoTM.png"
-                 style="max-width: 100%;"/>
-          </a>
-        </div>
-        <h1>${data.station.name}</h1>
-      </header>
-      <div class="tm-realtime-last-measure">
-        <span class="tm-realtime-label tm-realtime-label-last-measure">${this.label(el, 'lastMeasure')}</span>
-        <time>${moment(data.datetime).locale('it').format(datetimeFormat)}</time>
-      </div>
-      <div class="tm-realtime-data">
-        <div class="tm-realtime-temperature">
-          <span class="tm-realtime-label tm-realtime-label-temperature">${this.label(el, 'temperature')}</span>
-          <span class="tm-realtime-value tm-temp-${tempClass}">${data.temperature} °C</span>
-        </div>
-        <div class="tm-realtime-rh">
-          <span class="tm-realtime-label tm-realtime-label-rh">${this.label(el, 'rh')}</span>
-          <span class="tm-realtime-value">${data.relative_humidity} %</span>
-        </div>
-        <div class="tm-realtime-pressure">
-          <span class="tm-realtime-label tm-realtime-label-pressure">${this.label(el, 'pressure')}</span>
-          <span class="tm-realtime-value">${data.pressure} hPa</span>
-        </div>
-        <div class="tm-realtime-wind">
-          <span class="tm-realtime-label tm-realtime-label-wind">${this.label(el, 'wind')}</span>
-          <span class="tm-realtime-value">
-            ${data.wind_strength} km/h
-            <span class="${this.windDirClass(data.wind_dir)}">${data.wind_dir} deg</span>
-          </span>
-        </div>
-        <div class="tm-realtime-rain-rate">
-          <span class="tm-realtime-label tm-realtime-label-rain-rate">${this.label(el, 'rainRate')}</span>
-          <span class="tm-realtime-value">${data.rain_rate} mm/h</span>
-        </div>
-        <div class="tm-realtime-rain">
-          <span class="tm-realtime-label tm-realtime-label-rain">${this.label(el, 'rain')}</span>
-          <span class="tm-realtime-value">${data.rain} mm</span>
-        </div>
-      </div>
-    `
+    let type = $(el).attr('type')
 
-    $(el).replaceWith(wrapper.html(tpl))
+    let widget
+    if (type === 'default') {
+      widget = new DefaultTMRealtimeWidget()
+    }
+
+    $(el).replaceWith(widget.render(el, data))
 
     // callback
     let cb = $(el).attr('onReady') || null
     if (cb) {
-      execFunctionByName(cb, window, wrapper)
+      execFunctionByName(cb, window, widget)
     }
   }
+
 }
 
 // check requirements
---
+++ src/widgets/abstract.js
@@ -0,0 +1,43 @@
+/**
+ * Parent Widget Realtime Class
+ *
+ * every widget specifi class inherits from this one
+ */
+export default class TMRealtimeWidget {
+
+  defaultLabels = {
+    lastMeasure: 'ultima rilevazione',
+    temperature: 'temperatura',
+    rh: 'umidità relativa',
+    pressure: 'pressione',
+    wind: 'vento',
+    rainRate: 'intensità precipitazioni',
+    rain: 'accumulo precipitazioni'
+  }
+
+  label (el, l) {
+    return $(el).attr(l + 'Label') || this.defaultLabels[l]
+  }
+
+  windDirClass (dir) {
+    if (dir > 337) {
+      return 'direction-up'
+    } else if (dir > 293) {
+      return 'direction-up-left'
+    } else if (dir > 248) {
+      return 'direction-left'
+    } else if (dir > 202) {
+      return 'direction-down-left'
+    } else if (dir > 158) {
+      return 'direction-down'
+    } else if (dir > 113) {
+      return 'direction-down-right'
+    } else if (dir > 68) {
+      return 'direction-right'
+    } else if (dir > 23) {
+      return 'direction-up-right'
+    } else {
+      return 'direction-up'
+    }
+  }
+}
